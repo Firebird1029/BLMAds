c2bca1e5dceecad2daad6c5aeec7b0e3d8a5956e
Brandon <6111102+Firebird1029@users.noreply.github.com>
2020-07-10 21:36:29 -1000

    Inject our Adsense

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -10,9 +10,33 @@ chrome.extension.sendMessage({}, function (response) {
 			$(".adsbygoogle").each((i, el) => {
 				$(el).remove();
 			});
-			$("head").append(
-				`<script data-ad-client="ca-pub-8413947106743880" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`
-			);
+			// this function will work cross-browser for loading scripts asynchronously
+			// https://stackoverflow.com/questions/7718935/load-scripts-asynchronously#7719185
+			function loadScript(src, callback) {
+				var s, r, t;
+				r = false;
+				s = document.createElement("script");
+				s.id = "blm-ad-1";
+				s.type = "text/javascript";
+				s.src = src;
+				s.onload = s.onreadystatechange = function () {
+					//console.log( this.readyState ); //uncomment this line to see which ready states are called.
+					if (!r && (!this.readyState || this.readyState == "complete")) {
+						r = true;
+						callback();
+					}
+				};
+				t = document.getElementsByTagName("script")[0];
+				t.parentNode.insertBefore(s, t);
+			}
+			loadScript("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", () => {
+				console.log("done");
+				$("#blm-ad-1").attr("data-ad-client", "ca-pub-8413947106743880");
+			});
+
+			// $("body").append(
+			// 	`<script data-ad-client="ca-pub-8413947106743880" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>`
+			// );
 			// ----------------------------------------------------------
 		}
 	}, 10);
